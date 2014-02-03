07656b88a70ba00a2c79cd6bb240c5353d2007ac
Francisco López <donflopez@gmail.com>
2014-02-03 17:36:02 +0100

    Multiples graph bugfix

--- easy-d3.js
+++ easy-d3.js
@@ -18,66 +18,64 @@ if(Meteor.isClient){
   });
 
 function bar(opts){
-  if(!this.rendered){
+  if(!this['r'+opts.name]){
       //Creating permanent d3 objects between reloads
-      this.d3b = {};   //Margins
-      this.d3b.margin = {top: 20, right: 5, bottom: 30, left: 0},
-      console.log('Bar func: '+opts.name);
+      this[opts.name] = {};   //Margins
+      this[opts.name].margin = {top: 20, right: 5, bottom: 30, left: 0},
       //Width from original object for responsive design
-      this.d3b.width = $('#d3'+opts.name).width() - this.d3b.margin.left - this.d3b.margin.right,
-      this.d3b.height = 120 - this.d3b.margin.top - this.d3b.margin.bottom;
+      this[opts.name].width = $('#d3'+opts.name).width() - this[opts.name].margin.left - this[opts.name].margin.right,
+      this[opts.name].height = 120 - this[opts.name].margin.top - this[opts.name].margin.bottom;
 
       //Ordinal escale X
-      this.d3b.x = d3.scale.ordinal()
-        .rangeRoundBands([0, this.d3b.width], .02);
-      console.log(d3);
+      this[opts.name].x = d3.scale.ordinal()
+        .rangeRoundBands([0, this[opts.name].width], .02);
       //Linear scale Y
-      this.d3b.y = d3.scale.linear()
-        .range([this.d3b.height-2, 0]);
+      this[opts.name].y = d3.scale.linear()
+        .range([this[opts.name].height-2, 0]);
 
       //Color for bar
-      this.d3b.color = d3.scale.category10();
+      this[opts.name].color = d3.scale.category10();
 
-      this.d3b.xAxis = d3.svg.axis()
-        .scale(this.d3b.x)
+      this[opts.name].xAxis = d3.svg.axis()
+        .scale(this[opts.name].x)
         .orient("bottom");
 
-      this.d3b.yAxis = d3.svg.axis()
-          .scale(this.d3b.y)
+      this[opts.name].yAxis = d3.svg.axis()
+          .scale(this[opts.name].y)
           .orient("left")
           .ticks(10);
 
-      this.d3b.svg = d3.select('#d3'+opts.name)
-          .attr("width", this.d3b.width + this.d3b.margin.left + this.d3b.margin.right)
-          .attr("height", this.d3b.height + this.d3b.margin.top + this.d3b.margin.bottom)
+      this[opts.name].svg = d3.select('#d3'+opts.name)
+          .attr("width", this[opts.name].width + this[opts.name].margin.left + this[opts.name].margin.right)
+          .attr("height", this[opts.name].height + this[opts.name].margin.top + this[opts.name].margin.bottom)
         .append("g")
           .attr("class", "wrapper")
-          .attr("transform", "translate(" + this.d3b.margin.left + "," + this.d3b.margin.top + ")");
-      this.rendered=true;
+          .attr("transform", "translate(" + this[opts.name].margin.left + "," + this[opts.name].margin.top + ")");
+      this['r'+opts.name]=true;
 
-      this.d3b.svg.append("g")
+      this[opts.name].svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + this.d3b.height + ")")
-        .call(this.d3b.xAxis);
+        .attr("transform", "translate(0," + this[opts.name].height + ")")
+        .call(this[opts.name].xAxis);
     }
 
     //Setting keys
     var keys = _.map(opts.data, function(data, key){
       return data.key;
     });
-    this.d3b.color.domain(keys);
+    this[opts.name].color.domain(keys);
 
     // setting data
     var data = opts.data;
 
-    this.d3b.x.domain(keys);
-    this.d3b.y.domain([0, d3.max(data, function(d) { return d.size; })]);
+    this[opts.name].x.domain(keys);
+    this[opts.name].y.domain([0, d3.max(data, function(d) { return d.size; })]);
 
-    var bar_selector = this.d3b.svg.selectAll(".bar")
+    var bar_selector = this[opts.name].svg.selectAll(".bar")
       .data(data, function (d) {return d.key});
-    var text_selector = this.d3b.svg.selectAll(".bar_text")
+    var text_selector = this[opts.name].svg.selectAll(".bar_text")
       .data(data, function (d) {return d.key});
-    this.d3b.svg.selectAll('.x').call(this.d3b.xAxis);
+    this[opts.name].svg.selectAll('.x').call(this[opts.name].xAxis);
     var that = this;
 
     bar_selector
@@ -86,11 +84,11 @@ function bar(opts){
     bar_selector
       .transition()
       .duration(100)
-      .attr("x", function(d) { return that.d3b.x(d.key);})
-      .attr("width", that.d3b.x.rangeBand())
-      .attr("y", function(d) { return that.d3b.y(d.size); })
-      .attr("height", function(d) { return that.d3b.height - that.d3b.y(d.size); })
-      .style("fill", function(d) { return that.d3b.color(d.key);});
+      .attr("x", function(d) { return that[opts.name].x(d.key);})
+      .attr("width", that[opts.name].x.rangeBand())
+      .attr("y", function(d) { return that[opts.name].y(d.size); })
+      .attr("height", function(d) { return that[opts.name].height - that[opts.name].y(d.size); })
+      .style("fill", function(d) { return that[opts.name].color(d.key);});
 
     text_selector
       .enter().append("text")
@@ -99,11 +97,11 @@ function bar(opts){
       .transition()
       .duration(100)
       .attr()
-      .attr("x", function(d) { return that.d3b.x(d.key);})
+      .attr("x", function(d) { return that[opts.name].x(d.key);})
       .attr('font-size', '14px')
-      .attr("y", function(d) { return that.d3b.y(d.size) - 2; })
+      .attr("y", function(d) { return that[opts.name].y(d.size) - 2; })
       .text(function(d) {return d.size;})
-      .attr("height", function(d) { return that.d3b.height - that.d3b.y(d.size); });
+      .attr("height", function(d) { return that[opts.name].height - that[opts.name].y(d.size); });
 };
 
 };
